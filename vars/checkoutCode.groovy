4286d723338a5bd05893b3bdfe080f18c34a8810
Pannapon Terdchanakul <pannapon@entercorp.net>
2023-12-11 15:27:43 +0700

    add git scm

--- vars/checkoutCode.groovy
+++ vars/checkoutCode.groovy
@@ -1,6 +1,19 @@
-def call(String repoUrl, String branch){
-   def workingDir = "${env.WORKSPACE}"
-   sh "git clone ${repoUrl} ${workingDir}"
-   sh "git checkout ${branch}"
-   return workingDir
+// def call(String repoUrl, String branch){
+//    def workingDir = "${env.WORKSPACE}"
+//    sh "git clone ${repoUrl} ${workingDir}"
+//    sh "git checkout ${branch}"
+//    return workingDir
+// }
+
+
+/**
+  * Git Clone Repository and Checkout
+  *
+  * @return scmVars Object
+  */
+def call(String repoUrl, String branch) {
+  stage('Clone repository') {
+    scmVars = checkout scm
+  }
+  return scmVars
 }
